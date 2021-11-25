a6e2fc3fdea7b6e53b08c12c0fa59e34dc13ea78
Duane92222 <duane.estrade@laposte.net>
2021-11-25 12:32:47 +0100

    Ajout de l'interface graphique

---
+++ model/Couleur.java
@@ -0,0 +1,9 @@
+package src;
+
+public enum Couleur {
+	
+	BLANC,
+	NOIR
+	;
+
+}
--- model/Pion.java
+++ model/Pion.java
@@ -1,54 +1,66 @@
 package model;
 
-public class Pion {
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
 
-    int x,y;
-	char couleur;
+import javax.swing.JPanel;
 
 
+public class Pion extends JPanel {
+
+	private static final long serialVersionUID = 1436178861615738480L;
+
+	private Couleur couleur;
+	private boolean monte;
 
 
-	public Pion(int x, int y, char couleur) {
-		this.x = x;
-		this.y = y;
+	public Pion(Couleur couleur, boolean monte) {
+		this.monte=monte;
 		this.couleur = couleur;
+		setOpaque(false);
+		switch (couleur) {
+		case BLANC :
+			setForeground(Color.WHITE);
+			setBackground(new Color(220, 220, 220));
+			break;
+		case NOIR :
+			setForeground(new Color(70, 70, 70));
+			setBackground(new Color(200, 200, 200));
+			break;
+		}
+	}
+	
+	//Permet d'afficher l'interface graphique correctement
+	@Override
+	public void paintComponent(Graphics g){
+		Paint paint;
+		Graphics2D g2d;
+		if (g instanceof Graphics2D) {
+			g2d = (Graphics2D) g;
+		}
+		else {
+			System.out.println("Error");
+			return;
+		}
+		paint = new GradientPaint(0,0, getBackground(), getWidth(), getHeight(), getForeground());
+		g2d.setPaint(paint);
+		g.fillOval(5, 5, getWidth()-10, getHeight()-10);
+
 	}
 
-
-
-	public int getX() {
-		return x;
-	}
-
-
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-
-
-	public int getY() {
-		return y;
-	}
-
-
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-
-
-	public char getCouleur() {
+	public Couleur getCouleur() {
 		return couleur;
 	}
 
-
-
-	public void setCouleur(char nom) {
-		this.couleur = nom;
+	public boolean isMonte() {
+		return monte;
 	}
 
-	
+	public void setMonte(boolean monte) {
+		this.monte = monte;
+	}
+
 }
